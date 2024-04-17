c1d21e94f306b11a83058b053484262d1cb8bee5
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-04-17 01:36:10 +0000

    [Creación de list todo] [Creación de fetch en el home.jsx] [Creación de items, eliminación de items] [Métodos utilizados GET, PUT, DELETE]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,28 +9,75 @@ const Home = () => {
 
 	const [todo, setTodo] = useState("")
 	const [items, setItems] = useState([]);
+	const todosURL = "https://playground.4geeks.com/todo/"
+	function getTodos() {
+		fetch(todosURL + 'users/heandy', {
+			method: "GET"
+		}) 
+		.then((response)=>{console.log(response)
+			return response.json()
+		})
+		.then((data)=>{setItems(data.todos)})
+		.catch((error)=>{error})
+	}
 
 
+	let newTodo = {
+		"label": todo
+	}
+
+	const addTodo = () => { 
+		fetch(todosURL + 'todos/heandy', {
+			method: "POST",
+			body: JSON.stringify(newTodo),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Error al agregar una nueva tarea');
+			}
+			return response.json();
+		})
+		.then((data) => {
+			setItems([...items, data]); // Utiliza los datos devueltos por el servidor
+			setTodo(''); // Limpia el estado de todo
+		})
+			.catch(error => error)
+	}
 
 
 	const handleKeyPress = (event) => {
 		if (event.key === 'Enter') {
-			addItem();
+			addTodo();
 		}
 	};
 
-	const addItem = () => {
-		if (todo.trim() !== '') {
-			setItems([...items, todo]);
-			setTodo('');
-		}
-	};
-
+	
 	const removeItem = (index) => {
-		const updatedItems = [...items];
-		updatedItems.splice(index, 1);
-		setItems(updatedItems);
+		let itemToDelete = items[index]
+		console.log(itemToDelete);
+		fetch(`${todosURL}todos/${itemToDelete.id}`, {
+			method: "DELETE",
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Error al eliminar una nueva tarea');
+			} else if (response.status == 204) {
+				getTodos();
+			} 
+			
+		})
+		.catch(error => error)
+		
 	};
+	useEffect(()=> {
+		getTodos();
+	}, [])
 
 	return (
 		<div className="container text-center" style={{ width: "500px" }}>
@@ -48,12 +95,12 @@ const Home = () => {
 				<div className={`row shadow-lg py-2 mx-1 ${items.length === 0 ? "opacity-0" : "opacity-100"}`} >
 					<div className="col">
 						{items.map((item, index) => (
-							<p className={`d-flex justify-content-between px-3 border-bottom mt-2  esc`} key={index}>{item}
-								<button type="button" className={`btn btn-primary px-2 py-1 mb-2 `} onClick={() => removeItem(index)}><i class="fa-solid fa-x"></i></button>
-
-							</p>
-
+							 <p className={`d-flex justify-content-between px-3 border-bottom mt-2  esc`} key={index}>{item.label}
+							 <button type="button" className={`btn btn-primary px-2 py-1 mb-2 `} onClick={() => removeItem(index)}><i className="fa-solid fa-x"></i></button>
+							 </p>
+							
 						))}
+						
 					</div>
 					<span className={`d-flex justify-content-start fs-6 px-3 text-secondary py-0 ${items.length === 0 ? "opacity-0" : "opacity-100"}`}>{items.length} Tareas restantes</span>
 				</div>
